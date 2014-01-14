39eba11f8ee3a86b3880386191a6192b3ecb3942
João Paulo Faria <jpfaria@gmail.com>
2014-01-14 15:57:36 -0200

    .

--- src/br/com/jpfaria/android/MainActivity.java
+++ src/br/com/jpfaria/android/MainActivity.java
@@ -1,16 +1,37 @@
 package br.com.jpfaria.android;
 
+import static com.udinic.accounts_authenticator_example.authentication.AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS;
 
-import android.os.Bundle;
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
 import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import com.udinic.accounts_authenticator_example.authentication.AccountGeneral;
 
 public class MainActivity extends Activity {
 
+	private AccountManager mAccountManager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mAccountManager = AccountManager.get(this);
+		
+		findViewById(R.id.btnAddAccount).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewAccount(AccountGeneral.ACCOUNT_TYPE, AccountGeneral.AUTHTOKEN_TYPE_FULL_ACCESS);
+            }
+        });
 	}
 
 	@Override
@@ -19,5 +40,34 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void addNewAccount(String accountType, String authTokenType) {
+        final AccountManagerFuture<Bundle> future = mAccountManager.addAccount(accountType, authTokenType, null, null, this, new AccountManagerCallback<Bundle>() {
+            @Override
+            public void run(AccountManagerFuture<Bundle> future) {
+                try {
+                    Bundle bnd = future.getResult();
+                    showMessage("Account was created");
+                    Log.d("udinic", "AddNewAccount Bundle is " + bnd);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    showMessage(e.getMessage());
+                }
+            }
+        }, null);
+    }
+	
+	private void showMessage(final String msg) {
+    	if (TextUtils.isEmpty(msg))
+            return;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 }
